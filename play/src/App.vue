bd72f072c7eb47e17f647739a5f126957304ff04
zukangHuang <zukang9520@163.com>
2022-08-12 11:37:57 +0800

    refactor: play

---
+++ play/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <h1>Hello Nice UI</h1>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+    text-align: center;
+    line-height: 70vh;
+}
+</style>
